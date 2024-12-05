1a50ac52d5e713fc84d95e7cb6f0c511b87d7aca
Minh Nhat Le <nhatminh.vnm@gmail.com>
2024-12-04 22:48:17 -0500

    updated gpt4o directprobe

--- scripts/direct_probing/Openai_direct_probing.py
+++ scripts/direct_probing/Openai_direct_probing.py
@@ -2,8 +2,7 @@ from bs4 import BeautifulSoup
 from openai import OpenAI
 import pandas as pd
 import os
-import re
-from unidecode import unidecode
+
 
 client = OpenAI(
     api_key=os.environ['OpenAI_API_KEY'],
@@ -19,35 +18,42 @@ def extract_output(html):
     return None
 
 
-def predict(lang, passage):
-    few_shot_examples = {
+def predict(lang, passage, mode="unshuffled", prompt_setting="zero-shot"):
+    
+    demonstrations = {
         "es": {
-            "example_passage": "Lord Henry alzó las cejas y lo miró con asombro a través de las delgadas volutas de humo que, al salir de su cigarrillo con mezcla de opio, se retorcían adoptando extrañas formas.",
-            "example_output": '"title": "The Picture of Dorian Gray","author": "Oscar Wilde"'
+            "unshuffled": "Hemos de agregar que quemaba tan hondamente el pecho de Hester, que quizá había mayor verdad en el rumor que lo que nuestra moderna incredulidad nos permite aceptar.",
+            "shuffled": "lo Hemos quemaba de verdad nos moderna rumor hondamente que que el quizá tan en el mayor había que agregar pecho Hester, que aceptar. de incredulidad permite nuestra"
         },
         "tr": {
-            "example_passage": "Kendimden pek çok şey kattım buna.” Lord Henry divana boylu boyunca uzanarak güldü.",
-            "example_output": '"title": "The Picture of Dorian Gray","author": "Oscar Wilde"'
+            "unshuffled": "Ve Hester'ın göğsünü o kadar derinden yaktı ki, belki de modern şüphemizin kabul etmeye meyilli olmadığı söylentide daha fazla gerçeklik vardı.",
+            "shuffled": "ki, yaktı göğsünü gerçeklik vardı. meyilli söylentide belki fazla Hester'ın derinden olmadığı Ve kadar şüphemizin de kabul modern etmeye daha o"
         },
         "vi": {
-            "example_passage": "Oliver không hề thích thú kiểu đùa giỡn này và chuẩn bị thổ lộ sự bất bình của mình với hai anh bạn",
-            "example_output": '"title": "Oliver Twist","author": "Charles Dickens"'
+            "unshuffled": "Và chúng ta tất phải thuật lại rằng nó đã nung đốt thành dấu hằn vào ngực Hester sâu đến nỗi có lẽ trong lời đồn kia có nhiều phần sự thực hơn là đầu óc đa nghi của chúng ta trong thời hiện đại có thể sẵn sàng thừa nhận.",
+            "shuffled": "ta phải thuật trong ta trong lẽ thể đại nỗi có nhận. nung đa hằn nghi đốt đồn lời vào dấu sâu Và hơn có sự hiện Hester của có phần thực kia ngực sẵn chúng tất thời nhiều sàng chúng đầu rằng đến là lại thừa đã óc nó thành"
         },
         "en": {
-            "example_passage": "I am afraid I must be going, Basil.",
-            "example_output": '"title": "The Picture of Dorian Gray", "author": "Oscar Wilde"'
+            "unshuffled": "And we must needs say, it seared Hester's bosom so deeply, that perhaps there was more truth in the rumor than our modern incredulity may be inclined to admit.",
+            "shuffled": "admit. say, to inclined that the be more must so than it may needs modern we in rumor was deeply, incredulity perhaps our seared bosom there Hester's And truth"
         }
     }
 
-    example = few_shot_examples.get(lang)
-
-    text_template = """
-        You are provided with a passage in {lang}. Your task is to carefully read and determine which book this passage originates from and who the author is. You must make a guess, even if you are uncertain.
-
+    demo = demonstrations.get(lang)[mode]
+       
+    demo_prompt = ""
+    if prompt_setting != "zero-shot":
+        demo_prompt = f"""
+        
         Here is an example:
-        <passage>{example_passage}</passage>
-        <output>{example_output}</output>
-
+        <passage>{demo}</passage>
+        <output>"title": "The Scarlet Letter","author": "Nathaniel Hawthorne"</output>
+        
+        """
+        
+    prompt = f"""
+        You are provided with a passage in {lang}. Your task is to carefully read and determine which book this passage originates from and who the author is. You must make a guess, even if you are uncertain.
+        {demo_prompt}
         Here is the passage:
         <passage>{passage}</passage>
 
@@ -56,17 +62,12 @@ def predict(lang, passage):
     """
 
     completion = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-2024-11-20",
 
         max_tokens=100,
 
         messages=[
-            {"role": "user", "content": text_template.format(
-                lang=lang,
-                example_passage=example["example_passage"],
-                example_output=example["example_output"],
-                passage=passage
-            )}
+            {"role": "user", "content": prompt}
         ],
         temperature=0.0
     )
@@ -79,18 +80,21 @@ def predict(lang, passage):
     return completion.choices[0].message.content
 
 
-def direct_probe(csv_file_name, book_title, book_author):
+def direct_probe(csv_file_name, book_title, prompt_setting="zero-shot"):
     try:
         df = pd.read_csv(csv_file_name)
 
         for language in df.columns:
-            if language != 'Entity':
+            if language != 'Single_ent':
                 print(f'Running {language}')
                 output = []
                 passage_no = 0
                 for i in range(len(df)):
                     passage = df[language].iloc[i]
-                    content = predict(language, passage)
+                    if "shuffled" in language: 
+                        content = predict(language, passage, "shuffled", prompt_setting)
+                    else:
+                        content = predict(language, passage, "unshuffled", prompt_setting)
                     output.append(content)
                     print(f'{passage_no}: {content}')
                     passage_no += 1
@@ -98,7 +102,7 @@ def direct_probe(csv_file_name, book_title, book_author):
                 output_col = pd.Series(output)
                 df.insert(index_of_language + 1, f"{language}_results", output_col)
 
-        df.to_csv(f"{book_title}_direct_probe_gpt4o.csv", index=False, encoding='utf-8')
+        df.to_csv(f"{book_title}_direct_probe_gpt-4o-2024-11-20.csv", index=False, encoding='utf-8')
     except Exception as e:
         print(f'Error: {e}')
 
@@ -110,15 +114,10 @@ def get_folder_names(directory):
             folder_names.append(item)
     return folder_names
 
-def read_txt_file(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-    return content.strip()
-
 if __name__ == "__main__":
     titles = get_folder_names('/Prompts')
     
     for title in titles:
         print(f'----------------- running {title} -----------------')
-        direct_probe(csv_file_name=f"/Prompts/{title}/{title}_ner.csv", book_title=title, book_author=read_txt_file(f'/Prompts/{title}/author.txt'))
+        direct_probe(csv_file_name=f"/Prompts/{title}/{title}_filtered.csv", book_title=title,  prompt_setting="zero-shot") # modify the prompt setting here
             
