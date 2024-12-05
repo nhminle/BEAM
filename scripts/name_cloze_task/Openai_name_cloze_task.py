ebdf69fae549dd66bfaf965897ed474e8e046954
Minh Nhat Le <nhatminh.vnm@gmail.com>
2024-12-04 23:20:58 -0500

    update gpt4o dirprobe

--- scripts/name_cloze_task/Openai_name_cloze_task.py
+++ scripts/name_cloze_task/Openai_name_cloze_task.py
@@ -2,51 +2,58 @@ from bs4 import BeautifulSoup
 from openai import OpenAI
 import pandas as pd
 import os
-from unidecode import unidecode
+
 
 client = OpenAI(
     api_key=os.environ['OpenAI_API_KEY'],
 )
 
+
 def extract_output(html):
     soup = BeautifulSoup(html, 'html.parser')
-
     name_tag = soup.find('name')
-
     if name_tag:
         return name_tag.decode_contents()
 
     return None
 
-def predict(lang, passage):
-    few_shot_examples = {
+
+def predict(lang, passage, mode="unshuffled", prompt_setting="zero-shot"):
+    
+    demonstrations = {
         "es": {
-            "example_passage": "Creo que incluso [MASK] estaría de acuerdo con tu opinión",
-            "example_name": "Kafka"
+            "unshuffled": "Hemos de agregar que quemaba tan hondamente el pecho de [MASK], que quizá había mayor verdad en el rumor que lo que nuestra moderna incredulidad nos permite aceptar.",
+            "shuffled": "lo Hemos quemaba de verdad nos moderna rumor hondamente que que el quizá tan en el mayor había que agregar pecho [MASK], que aceptar. de incredulidad permite nuestra"
         },
         "tr": {
-            "example_passage": "Sadece [MASK] değil tüm grup hayduttu ve diğer insanlardan ayrı yaşıyorlardı",
-            "example_name": "Robin"
+            "unshuffled": "Ve [MASK]'ın göğsünü o kadar derinden yaktı ki, belki de modern şüphemizin kabul etmeye meyilli olmadığı söylentide daha fazla gerçeklik vardı.",
+            "shuffled": "ki, yaktı göğsünü gerçeklik vardı. meyilli söylentide belki fazla [MASK]'ın derinden olmadığı Ve kadar şüphemizin de kabul modern etmeye daha o"
         },
         "vi": {
-            "example_passage": "Rõ ràng đây là điều rất bình thường, chẳng có ai ăn bánh mà lại bị to ra hay bé đi cả, nhưng [MASK] đang quá trông chờ vào những điều bất thường nên khi cái điều bình thường đó xảy ra đã khiến cô cảm thấy cuộc đời sao mà ảm đạm và đáng chán.",
-            "example_name": "Alice"
+            "unshuffled": "Và chúng ta tất phải thuật lại rằng nó đã nung đốt thành dấu hằn vào ngực [MASK] sâu đến nỗi có lẽ trong lời đồn kia có nhiều phần sự thực hơn là đầu óc đa nghi của chúng ta trong thời hiện đại có thể sẵn sàng thừa nhận.",
+            "shuffled": "ta phải thuật trong ta trong lẽ thể đại nỗi có nhận. nung đa hằn nghi đốt đồn lời vào dấu sâu Và hơn có sự hiện [MASK] của có phần thực kia ngực sẵn chúng tất thời nhiều sàng chúng đầu rằng đến là lại thừa đã óc nó thành"
         },
         "en": {
-            "example_passage": "Stay gold, [MASK], stay gold.",
-            "example_name": "Ponyboy"
+            "unshuffled": "And we must needs say, it seared [MASK]'s bosom so deeply, that perhaps there was more truth in the rumor than our modern incredulity may be inclined to admit.",
+            "shuffled": "admit. say, to inclined that the be more must so than it may needs modern we in rumor was deeply, incredulity perhaps our seared bosom there [MASK]'s And truth"
         }
     }
 
-    example = few_shot_examples.get(lang)
-
-    text_template = """
-        You are provided with a passage from a book. Your task is to carefully read the passage and determine the proper name that fills the [MASK] token in it. This name is exactly one word long, and is a proper name (not a pronoun or any other word). You must make a guess, even if you are uncertain:
-
+    demo = demonstrations.get(lang)[mode]
+    
+    demo_prompt = ""
+    if prompt_setting != "zero-shot":
+        demo_prompt = f"""
+        
         Here is an example:
-        <passage>{example_passage}</passage>
-        <name>{example_name}</name>
-
+        <passage>{demo}</passage>
+        <output>"title": "The Scarlet Letter","author": "Nathaniel Hawthorne"</output>
+        
+        """
+    
+    prompt = f"""
+       You are provided with a passage from a book. Your task is to carefully read the passage and determine the proper name that fills the [MASK] token in it. This name is a proper name (not a pronoun or any other word). You must make a guess, even if you are uncertain:
+        {demo_prompt}
         Here is the passage:
         <passage>{passage}</passage>
 
@@ -55,16 +62,12 @@ def predict(lang, passage):
     """
 
     completion = client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-2024-11-20",
 
         max_tokens=100,
 
         messages=[
-            {"role": "user", "content": text_template.format(
-                example_passage=example["example_passage"],
-                example_output=example["example_output"],
-                passage=passage
-            )}
+            {"role": "user", "content": prompt}
         ],
         temperature=0.0
     )
@@ -76,18 +79,21 @@ def predict(lang, passage):
         print(completion.choices[0].message.content)
     return completion.choices[0].message.content
 
-def name_cloze_task(csv_file_name, book_title):
+def name_cloze_task(csv_file_name, book_title, prompt_setting="zero-shot"):
     try:
         df = pd.read_csv(csv_file_name)
 
-        for col in df.columns:
-            if col != 'Entity':
-                print(f'///running {col}///')
+        for language in df.columns:
+            if language != 'Single_ent':
+                print(f'Running {language}')
                 output = []
                 for i in range(len(df)):
-                    masked_passage = df[col].iloc[i]
-                    content = predict(col, masked_passage)
-                    print(i, content)
+                    masked_passage = df[language].iloc[i]
+                    if "shuffled" in language: 
+                        content = predict(language, masked_passage, "shuffled", prompt_setting)
+                    else:
+                        content = predict(language, masked_passage, "unshuffled", prompt_setting)
+                    print(f'{i}: {content}')
                     output.append(content)
                 index_of_language = df.columns.get_loc(col)
                 guess_results = pd.Series(output)
