657ee48f4127797f5724826e969e0ae3f47cacd8
emirkaan5 <emirkorukluoglu@gmail.com>
2025-03-21 02:31:20 +0000

    extra results

--- scripts/slurm_prefix_eval.sh
+++ scripts/slurm_prefix_eval.sh
@@ -7,7 +7,7 @@
 #SBATCH --open-mode=truncate
 #SBATCH --mail-type=ALL       # notify you about the start and end of the job
 #SBATCH --mail-user=ekorukluoglu@umass.edu # Email to which notifications will be sent
-#SBATCH --array=0-11
+#SBATCH --array=0-4
 
 # Load any required modules here
 module load cuda/11.8
@@ -16,7 +16,7 @@ module load cuda/11.8
 source .venv/bin/activate
 
 # Define the models array
-models=('EuroLLM-9B-Instruct' 'OLMo-7B-0724-Instruct-hf' 'Llama-3.1-70B-Instruct' 'Llama-3.3-70B-Instruct' 'Llama-3.1-8B-Instruct' 'OLMo-2-1124-13B-Instruct' 'Qwen2.5-7B-Instruct-1M' 'OLMo-2-1124-7B-Instruct' 'Llama-3.1-8B-Instruct-quantized.w4a16' 'Llama-3.1-8B-Instruct-quantized.w8a16' 'Llama-3.1-70B-Instruct-quantized.w4a16' 'Llama-3.1-8B-Instruct-quantized.w8a16' 'Llama-3.1-405b')
+models=('gpt-4o-2024-11-20' 'Llama-3.1-70B-Instruct' 'Llama-3.1-70B-Instruct-quantized.w4a16' 'Llama-3.1-70B-Instruct-quantized.w8a16')
 
 # Get the model corresponding to this SLURM array index
 model=${models[$SLURM_ARRAY_TASK_ID]}
